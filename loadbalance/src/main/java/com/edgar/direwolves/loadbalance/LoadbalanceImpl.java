242e9559d2a0bac654cf72c5913cc3d46653dafb
Edgar <edgar615@gmail.com>
2017-07-30 22:12:19 +0800

    sta

--- loadbalance/src/main/java/com/edgar/direwolves/loadbalance/LoadbalanceImpl.java
+++ loadbalance/src/main/java/com/edgar/direwolves/loadbalance/LoadbalanceImpl.java
@@ -1,5 +1,11 @@
 package com.edgar.direwolves.loadbalance;
 
+import io.vertx.servicediscovery.Record;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * Created by Edgar on 2017/7/28.
  *
@@ -7,7 +13,23 @@ package com.edgar.direwolves.loadbalance;
  */
 class LoadbalanceImpl implements LoadBalance {
 
+  private ChooseStrategy strategy = ChooseStrategy.roundRobin();
+
+  private final List<ServiceFilter> filters = new ArrayList<>();
+
   public LoadBalance withStrategy(ChooseStrategy strategy) {
+    Objects.requireNonNull(strategy, "ChooseStrategy");
+    this.strategy = strategy;
+    return this;
+  }
+
+  public LoadBalance addFilter(ServiceFilter filter) {
+    Objects.requireNonNull(filter, "ServiceFilter");
+    filters.add(filter);
+    return this;
+  }
+
+  public Record choose() {
     return null;
   }
 }
---
+++ loadbalance/src/main/java/com/edgar/direwolves/loadbalance/loadBalanceStats.java
@@ -0,0 +1,7 @@
+package com.edgar.direwolves.loadbalance;
+
+/**
+ * Created by edgar on 17-7-29.
+ */
+public class loadBalanceStats {
+}
