a8d849f0debd6fd56d9d1520b04cbdab430c0e13
edgar615 <edgar615@gmail.com>
2017-09-07 16:59:02 +0800

    开始编写集成测试模块

--- standalone/src/test/java/com/edgar/direwolves/mock/DeviceHttpVerticle.java
+++ standalone/src/test/java/com/edgar/direwolves/mock/DeviceHttpVerticle.java
@@ -1,4 +1,4 @@
-package com.edgar.direwolves.standalone;
+package com.edgar.direwolves.mock;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
@@ -19,33 +19,36 @@ public class DeviceHttpVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
+//    DeviceStore store = new DeviceStore();
+//    initData(store);
+
     vertx.createHttpServer().requestHandler(req -> {
+      req.response().putHeader("Content-Type", "application/json");
       String url = req.path();
       if (url.equals("/devices") && req.method() == HttpMethod.GET) {
+        String start = req.getParam("start");
+        String limit = req.getParam("limit");
         JsonArray devices = new JsonArray();
-        devices.add(new JsonObject().put("id", 1))
-                .add(new JsonObject().put("id", 2));
-        req.response().putHeader("Content-Type", "application/json")
+        devices.add(new JsonObject().put("id", start))
+                .add(new JsonObject().put("id", limit));
+        req.response()
                 .end(devices.encode());
       }
       if (url.startsWith("/devices/") && req.method() == HttpMethod.GET) {
-        String id = url.substring("/devices/".length());
-        req.response().putHeader("Content-Type", "application/json")
-                .end(new JsonObject().put("id", id)
-                             .put("query", req.query()).encode());
+        int id = Integer.parseInt(url.substring("/devices/".length()));
+        req.response()
+                .end(new JsonObject().put("id", id).encode());
       }
       if (url.equals("/devices") && req.method() == HttpMethod.DELETE) {
-        req.response().putHeader("Content-Type", "application/json")
+        req.response()
                 .end(new JsonObject()
                              .put("result", "1")
                              .encode());
       }
       if (url.equals("/devices") && req.method() == HttpMethod.POST) {
-        req.response().putHeader("Content-Type", "application/json");
         req.bodyHandler(body -> req.response().end(body));
       }
       if (url.equals("/devices") && req.method() == HttpMethod.PUT) {
-        req.response().putHeader("Content-Type", "application/json");
         req.bodyHandler(body -> req.response().end(body));
       }
     }).listen(config().getInteger("http.port", 8080), ar -> {
@@ -55,6 +58,12 @@ public class DeviceHttpVerticle extends AbstractVerticle {
         startFuture.fail(ar.cause());
       }
     });
+  }
+
+  private void initData(DeviceStore store) {
+    for (int i = 0; i < 100; i++) {
+      store.add(new Device(i, "device-" + i));
+    }
   }
 
 }
