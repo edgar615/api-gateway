f9c89889885005483e34c512a39d391651b975ab
Edgar <edgar615@gmail.com>
2018-08-12 11:04:21 +0800

    remoteAddrPredicate

---
+++ core/src/main/java/com/github/edgar615/gateway/core/plugin/predicate/RemoteAddrRangePredicate.java
@@ -0,0 +1,37 @@
+package com.github.edgar615.gateway.core.plugin.predicate;
+
+import com.github.edgar615.gateway.core.dispatch.ApiContext;
+import com.github.edgar615.util.net.IPUtils;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+
+public class RemoteAddrRangePredicate implements ApiPredicate {
+  /**
+   * IP地址的开始值，用long表示
+   */
+  private final long start;
+
+  /**
+   * IP地址的结束值，用long表示
+   */
+  private final long end;
+
+
+  public RemoteAddrRangePredicate(long start, long end) {
+    Preconditions.checkArgument(start <= end);
+    Preconditions.checkArgument(start >= 0);
+    Preconditions.checkArgument(end <= 4294967295l);
+    this.start = start;
+    this.end = end;
+  }
+
+  public boolean test(ApiContext context) {
+    String clientIp = (String) context.variables().get("request_clientIp");
+    if (Strings.isNullOrEmpty(clientIp)) {
+      return false;
+    }
+    long ipNumber = IPUtils.ipToLong(clientIp);
+    return ipNumber >= start && ipNumber <= end;
+  }
+
+}
---
+++ core/src/test/java/com/github/edgar615/gateway/core/plugin/predicate/RemoteAddrHashPredicateTest.java
@@ -0,0 +1,38 @@
+package com.github.edgar615.gateway.core.plugin.predicate;
+
+import com.github.edgar615.gateway.core.dispatch.ApiContext;
+import com.github.edgar615.util.net.IPUtils;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import io.vertx.core.http.HttpMethod;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoteAddrHashPredicateTest {
+
+  @Test
+  public void testHash() {
+    System.out.println(IPUtils.ipToLong("192.168.1.3") % 100);
+    System.out.println(IPUtils.ipToLong("172.168.135.111") % 100);
+    System.out.println(IPUtils.ipToLong("192.168.1.5") % 100);
+    System.out.println(IPUtils.ipToLong("169.168.1.5") % 100);
+    RemoteAddrHashPredicate predicate = new RemoteAddrHashPredicate(60, 80);
+    Multimap<String, String> header = ArrayListMultimap.create();
+    ApiContext apiContext
+            = ApiContext.create(HttpMethod.GET, "/", header, ArrayListMultimap.create(), null);
+    apiContext.addVariable("request_clientIp", "192.168.1.3");
+    Assert.assertTrue(predicate.test(apiContext));
+
+    apiContext.addVariable("request_clientIp", "172.168.135.111");
+    Assert.assertTrue(predicate.test(apiContext));
+
+    apiContext.addVariable("request_clientIp", "192.168.1.5");
+    Assert.assertFalse(predicate.test(apiContext));
+
+    apiContext.addVariable("request_clientIp", "169.168.1.5");
+    Assert.assertFalse(predicate.test(apiContext));
+  }
+}
