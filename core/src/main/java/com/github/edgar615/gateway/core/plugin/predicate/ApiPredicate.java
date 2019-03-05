5d866e1076a615fb8af9b6480f8a8f0414b897f6
edgar615 <edgar615@gmail.com>
2019-03-05 23:06:01 +0800

    v1.2.0 first commit

--- core/src/main/java/com/github/edgar615/gateway/core/plugin/predicate/ApiPredicate.java
+++
@@ -1,8 +0,0 @@
-package com.github.edgar615.gateway.core.plugin.predicate;
-
-import com.github.edgar615.gateway.core.dispatch.ApiContext;
-
-import java.util.function.Predicate;
-
-public interface ApiPredicate extends Predicate<ApiContext> {
-}
